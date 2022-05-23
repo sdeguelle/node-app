9834e8097aa765d01410a39d44c9242fb0b72828
sdeguelle <sdeguelle@awenet.be>
2022-05-23 11:59:33 +0200

    Ident test

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,9 +2,9 @@ const app = require('../src/app')
 const supertest = require('supertest')
 const request = supertest(app)
 describe('/test endpoint', () => {
-it('should return a response', async () => {
-    const response = await request.get('/test')
-    expect(response.status).toBe(200)
-expect(response.text).toBe('Test page')
-  })
+    it('should return a response', async () => {
+        const response = await request.get('/test')
+        expect(response.status).toBe(200)
+        expect(response.text).toBe('Test page')
+    })
 })
